55979760fb2ebc23bdfd8b3cae61cef342b80022
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-11-26 15:09:38 -0500

    adds styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,28 +2,30 @@ function About(props) {
 
     
     return (
-        <div>
-            <h1>About</h1>
-            <p>Hi, my name is Ashley Swank, and I'm a software engineer. I spent the years from 2010 up until September of 2021 as a phlebotomist. 
-                the last six of those eleven years was working for myself as a private contractor. I decided to change careers to software engineering and development
-                when I realized I had maxed out my potential and hit the ceiling in the field I was in, and wanted to pursue something that aligned more with 
-                my personal interests, and gave me more room to grow. While working for myself though, I learned some very valuable tools that helped me on
-                this incredible journey into the tech industry. I learned about bottom lines, and how to represent myself, but more importantly I learned how 
-                to wake up every day and go do my job without anyone telling me to do it. I learned how quickly I can adapt and learn new ways of doing things.
-                I learned how important having a professional but empathetic relationship with patients and clients really is. I also learned more about who I was and what I wanted
-                out of life.
-            </p>
-            <p> When I realized I had hit the ceiling as a phlebotomist, I had to have a heart to heart with myself about what I wanted to do next and what moves I had to make to get there.
-                I have always loved computers whether it be building them or playing games on them. I absolutely love video games, and among some of my favorites are 
-                games where I could solve puzzles and problem solve. As an avid gamer, I have also always been interested in what makes the engines run.  
-                I am also an artist and I saw making the transition to software development as being a move that made the most sense for me, since I could not only explore 
-                my own creativity, but explore what I love the most about gaming as well: puzzles and problem solving. Software engineering straddled my interests so completely and perfectly,
-                that I found my answer. I set off by first taking a Front End Web development course through General Assembly and absolutely loved it, and it was because of that course
-                I enrolled in the Software Engineering Immersive bootcamp, also through General Assembly. I feel the education I have recieved gave me the tools needed 
-                to make my first steps into this new world. I cannot wait to get started at a new job and learn everything I can about it. I wake up excited every day 
-                at the world of possibilites now in front of me. I am enthusiastic and beyond willing to take the care and time needed to learn anything and everything, 
-                as well as the chance to work with wonderful and like-minded people. I hope I can continue to grow and put some great work in. 
-            </p>
+        <div class="about-body">
+            <div class="about-div">
+                <h1 class="about-text">About</h1>
+                <p class="about-p"> Hi, my name is Ashley Swank, and I'm a software engineer. I spent the years from 2010 up until September of 2021 as a phlebotomist. 
+                    the last six of those eleven years was working for myself as a private contractor. I decided to change careers to software engineering and development
+                    when I realized I had maxed out my potential and hit the ceiling in the field I was in, and wanted to pursue something that aligned more with 
+                    my personal interests, and gave me more room to grow. While working for myself though, I learned some very valuable tools that helped me on
+                    this incredible journey into the tech industry. I learned about bottom lines, and how to represent myself, but more importantly I learned how 
+                    to wake up every day and go do my job without anyone telling me to do it. I learned how quickly I can adapt and learn new ways of doing things.
+                    I learned how important having a professional but empathetic relationship with patients and clients really is. I also learned more about who I was and what I wanted
+                    out of life.
+                </p>
+                <p class="about-p"> When I realized I had hit the ceiling as a phlebotomist, I had to have a heart to heart with myself about what I wanted to do next and what moves I had to make to get there.
+                    I have always loved computers whether it be building them or playing games on them. I absolutely love video games, and among some of my favorites are 
+                    games where I could solve puzzles and problem solve. As an avid gamer, I have also always been interested in what makes the engines run.  
+                    I am also an artist and I saw making the transition to software development as being a move that made the most sense for me, since I could not only explore 
+                    my own creativity, but explore what I love the most about gaming as well: puzzles and problem solving. Software engineering straddled my interests so completely and perfectly,
+                    that I found my answer. I set off by first taking a Front End Web development course through General Assembly and absolutely loved it, and it was because of that course
+                    I enrolled in the Software Engineering Immersive bootcamp, also through General Assembly. I feel the education I have recieved gave me the tools needed 
+                    to make my first steps into this new world. I cannot wait to get started at a new job and learn everything I can about it. I wake up excited every day 
+                    at the world of possibilites now in front of me. I am enthusiastic and beyond willing to take the care and time needed to learn anything and everything, 
+                    as well as the chance to work with wonderful and like-minded people. I hope I can continue to grow and put some great work in. 
+                </p>
+            </div>
         </div>
     );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,25 +11,27 @@ function Projects(props) {
     return (
         <div>
             <h1>A few of my projects and builds</h1>
-            <div>
-                <a href="https://l2qee.csb.app/"><img class="thumbnail" src={lampshade} alt="lampshade" /></a>
-                <a href="https://codesandbox.io/s/final-project-forked-l2qee">The code</a>
-            </div>
-            <div>
-                <a href="https://meditrack-app.herokuapp.com/"><img class="thumbnail" src={meditrack} alt="meditrack" /></a>
-                <a href="https://github.com/alengysia/meditrack">The code</a>
-            </div>
-            <div>
-                <a href="https://quizzical-jepsen-f43607.netlify.app/"><img class="thumbnail" src={whatISthat} alt="meditrack" /></a>
-                <a href="https://github.com/alengysia/what-is-that-frontend">The code</a>
-            </div>
-            <div>
-                <a href="https://gk3q4.csb.app/"><img class="thumbnail" src={clock} alt="meditrack" /></a>
-                <a href="https://codesandbox.io/s/wonderful-orla-gk3q4">The code</a>
-            </div>
-            <div>
-                <a href="https://reverent-engelbart-e9df11.netlify.app/"><img class="thumbnail" src={drumkit} alt="meditrack" /></a>
-                <a href="https://codesandbox.io/s/wonderful-orla-gk3q4">The code</a>
+                <div class="projects-div">
+                    <div class="projects-card">
+                        <a href="https://l2qee.csb.app/"><img class="thumbnail" src={lampshade} alt="lampshade" /></a>
+                        <a href="https://codesandbox.io/s/final-project-forked-l2qee">The code</a>
+                    </div>
+                    <div class="projects-card">
+                        <a href="https://meditrack-app.herokuapp.com/"><img class="thumbnail" src={meditrack} alt="meditrack" /></a>
+                        <a href="https://github.com/alengysia/meditrack">The code</a>
+                    </div>
+                    <div class="projects-card">
+                        <a href="https://quizzical-jepsen-f43607.netlify.app/"><img class="thumbnail" src={whatISthat} alt="meditrack" /></a>
+                        <a href="https://github.com/alengysia/what-is-that-frontend">The code</a>
+                    </div>
+                    <div class="projects-card">
+                        <a href="https://gk3q4.csb.app/"><img class="thumbnail" src={clock} alt="meditrack" /></a>
+                        <a href="https://codesandbox.io/s/wonderful-orla-gk3q4">The code</a>
+                    </div>
+                    <div class="projects-card">
+                        <a href="https://reverent-engelbart-e9df11.netlify.app/"><img class="thumbnail" src={drumkit} alt="meditrack" /></a>
+                        <a href="https://codesandbox.io/s/wonderful-orla-gk3q4">The code</a>
+                    </div>
             </div>
         </div>
     );
