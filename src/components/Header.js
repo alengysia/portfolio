75b42c2408e52eea98163e5f4ba4c1740693e5ad
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-12-06 17:14:15 -0500

    mobile friendly styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ import { Link } from 'react-router-dom';
 function Header(props) {
     return (
       <nav className="nav">
-        <h1 class="nav-h1">Welcome to my portfolio</h1>
         <Link to="/">
-          <div class="home-nav">Home</div>
+        <h1 class="nav-h1">Welcome to my portfolio</h1>
         </Link>
       </nav>
     );
